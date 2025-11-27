8924d14d8e4e42107831869c1ae9dcd42c5a7e31
Twinkle-thomas <twinklethomas52@gmail.com>
2025-11-27 18:52:00 +1300

    1. Added delete functionality for the fitness application. 2. Did unit testing and pytest

--- api/views.py
+++ api/views.py
@@ -67,3 +67,9 @@ class ActivityDetailView(generics.RetrieveUpdateDestroyAPIView):
         self.perform_update(serializer)
         return Response({"detail": "Activity updated successfully!"}, status=status.HTTP_200_OK)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"detail": "Activity deleted successfully!"}, status=status.HTTP_200_OK)
+
+
